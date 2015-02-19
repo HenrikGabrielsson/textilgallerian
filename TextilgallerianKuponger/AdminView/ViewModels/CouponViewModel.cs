61d56c3e2a304d0e4f7094937ee37b3d68a78242
Henrik Gabrielsson <hg222aa@Ny106x08.lnu.se>
2015-02-19 16:20:22 +0100

    adminview: added customers to coupon-form

--- TextilgallerianKuponger/AdminView/ViewModels/CouponViewModel.cs
+++ TextilgallerianKuponger/AdminView/ViewModels/CouponViewModel.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Domain.Entities;
+using System.Net.Mail;
+using System.Text.RegularExpressions;
 
 namespace AdminView.ViewModel
 {
@@ -14,6 +16,38 @@ namespace AdminView.ViewModel
             {typeof (TotalSumAmountDiscount).FullName, "Köp för X:kr få Y:kr rabatt"},
             {typeof (TotalSumPercentageDiscount).FullName, "Köp för X:kr få Y:% rabatt"}
         };
+
+        public void CreateCustomers()
+        {
+            Customers = new List<Customer>();
+
+            string[] lines = CustomerString.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+
+
+            foreach (string line in lines)
+            {
+                Customer customer = new Customer();
+                //customer.CouponUses = 0;
+
+                Regex regex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+                Match match = regex.Match(line);
+                if (match.Success)
+                {
+                    customer.Email = line;
+                    Customers.Add(customer);
+                }
+
+                else if (true)
+                {
+                    customer.SocialSecurityNumber = line;
+                    Customers.Add(customer);
+                }
+            }
+        }
+
+        public string CustomerString { get; set; }
+
+        public List<Customer> Customers { get; set; }
 
         public String Type { get; set; }
         public Boolean CanBeCombined { get; set; }
